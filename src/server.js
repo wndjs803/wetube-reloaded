e48e6a69426be6e45aee403aa72d64aff2ca71b2
jueon <wndjs803@naver.com>
2023-02-06 19:17:58 +0900

    Import morgan

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
-  return res.end();
-};
-const handleLogin = (req, res) => {
-  return res.send("Login!");
+  return res.send("?");
 };
 
+app.use(logger);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
 
 const handleListening = () => console.log("wow");
 
