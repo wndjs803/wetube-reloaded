c92e73baadde93e3675771c1257296ac8c6ee0be
jueon <wndjs803@naver.com>
2023-02-07 14:56:30 +0900

    Create Routers

--- src/server.js
+++ src/server.js
@@ -5,13 +5,29 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-  return res.send("?");
-};
-
 app.use(logger);
-app.get("/", handleHome);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () => console.log("wow");
 
