78c46cdde97c45f3d55e0ad004dc2096bc48d7a2
jueon <wndjs803@naver.com>
2023-02-09 16:02:33 +0900

    partials, extending

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,9 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
+  res.render("watch", { pageTitle: "Watch" });
 };
 export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("Edit");
+  res.render("edit", { pageTitle: "Edit" });
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
