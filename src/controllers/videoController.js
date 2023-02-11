c1f32276519ef490df646b7eb574d33d5d1944f5
jueon <wndjs803@naver.com>
2023-02-11 18:12:17 +0900

    Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,7 @@
-const fakeUser = {
-  username: "Pooh",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 export const see = (req, res) => {
   res.render("watch", { pageTitle: "Watch" });
 };
