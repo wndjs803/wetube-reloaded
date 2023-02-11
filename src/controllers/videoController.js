67242940230b2ffc51750aecce1de0aba93caf53
jueon <wndjs803@naver.com>
2023-02-11 20:59:59 +0900

    Array database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,37 @@
+let videos = [
+  {
+    title: "First video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 2,
-    },
-    {
-      title: "Third video",
-      rating: 5,
-      comment: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 3,
-    },
-  ];
   res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-  res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("watch", { pageTitle: `Watch ${video.title}`, video });
 };
 export const edit = (req, res) => {
   res.render("edit", { pageTitle: "Edit" });
