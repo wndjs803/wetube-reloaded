a77cd95e7226eef52a54d98538496d88e8a28a45
jueon <wndjs803@naver.com>
2023-02-11 21:32:01 +0900

    POST

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -26,19 +26,16 @@ let videos = [
 ];
 
 export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home", videos });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  res.render("watch", { pageTitle: `Watch ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-export const edit = (req, res) => {
-  res.render("edit", { pageTitle: "Edit" });
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("Delete Video");
-};
+export const postEdit = (req, res) => {};
