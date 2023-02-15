ace5c931050d8c99c82ba60938c1aad75e3c720e
jueon <wndjs803@naver.com>
2023-02-15 17:12:29 +0900

    Exception and Validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -25,15 +25,17 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload",
+      errorMessage: error._message,
+    });
+  }
   return res.redirect("/");
 };
