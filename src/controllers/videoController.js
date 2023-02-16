476cb6ebc988ce638375a8dbab4c8e7e3d92229f
jueon <wndjs803@naver.com>
2023-02-16 20:05:16 +0900

    Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,7 +30,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags,
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -45,9 +45,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags: Video.formatHashtags(hashtags),
     });
   } catch (error) {
     return res.render("upload", {
