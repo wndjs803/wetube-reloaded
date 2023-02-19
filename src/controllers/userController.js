f12052f29de4f1763365f762b68b9264351633af
jueon <wndjs803@naver.com>
2023-02-19 15:32:05 +0900

    Create Account part one

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,10 @@
-export const join = (req, res) => res.send("Join");
+import Video from "../models/User";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  res.send("w");
+};
 export const edit = (req, res) => res.send("edit");
 export const del = (req, res) => res.send("delete");
 export const login = (req, res) => res.send("login");
