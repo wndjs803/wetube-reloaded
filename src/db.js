42dc1b2864ffd418e28fda1276613c851b1e2a70
jueon <wndjs803@naver.com>
2023-02-21 20:12:20 +0900

    Expiration and Secrets

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/wetube");
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.log("DB Error", error));
