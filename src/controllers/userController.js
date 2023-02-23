fe94d0f33a5858a10a5f87636ff2ef656eb4e8c1
jueon <wndjs803@naver.com>
2023-02-23 14:56:28 +0900

    Edit Profile GET

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -139,5 +139,11 @@ export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
-export const edit = (req, res) => res.send("edit");
+
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = (req, res) => {
+  res.send("edit");
+};
 export const see = (req, res) => res.send("see user");
--- src/middlewares.js
+++ src/middlewares.js
@@ -2,7 +2,7 @@ export const localMiddleware = (req, res, next) => {
   console.log(req.session);
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
-  res.locals.loggedInUser = req.session.user;
+  res.locals.loggedInUser = req.session.user || {};
   console.log(res.locals);
   next();
 };
