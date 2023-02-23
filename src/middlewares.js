10ae60fd1e5998379ac39260607457abb389dbea
jueon <wndjs803@naver.com>
2023-02-23 15:15:56 +0900

    protector and publickOnlyMiddleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -6,3 +6,19 @@ export const localMiddleware = (req, res, next) => {
   console.log(res.locals);
   next();
 };
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    next();
+  } else {
+    return res.redirect("/");
+  }
+};
