ee9d04ab503706570f1f346a3194867dce0ac344
jueon <wndjs803@naver.com>
2023-03-02 20:36:12 +0900

    Recording Video part one

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,40 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    console.log("end");
+    console.log(event);
+    console.log(event.data);
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 5000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true,
+    audio: false,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
