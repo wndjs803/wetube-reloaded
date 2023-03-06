fd29a6f78b3b35092988b38f54f3ef2f79a1550b
jueon <wndjs803@naver.com>
2023-03-06 14:38:50 +0900

    Rendering Comments

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,7 +12,8 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (video === null) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
@@ -150,10 +151,12 @@ export const createComment = async (req, res) => {
   if (!video) {
     return res.sendStatus(404);
   }
-  const comment = Comment.create({
+  const comment = await Comment.create({
     text,
     owner: user._id,
     video: id,
   });
+  video.comments.push(comment._id);
+  video.save();
   return res.sendStatus(201);
 };
